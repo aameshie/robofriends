7eb4dae4365da98fe622392348ce42a33db0ece2
Alvin Ameshie <dolsh3@gmail.com>
2022-10-17 01:35:59 -0400

    Updates

---
+++ precache-manifest.1637475be24ebe7629806f7f9265448f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce5cda77ae2bcd46e944",
+    "url": "/robofriends/static/css/main.8fb42261.chunk.css"
+  },
+  {
+    "revision": "ce5cda77ae2bcd46e944",
+    "url": "/robofriends/static/js/main.fafb9b8d.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "a66973f4af8d44bd9a7d",
+    "url": "/robofriends/static/css/2.656f6aca.chunk.css"
+  },
+  {
+    "revision": "a66973f4af8d44bd9a7d",
+    "url": "/robofriends/static/js/2.f57971e9.chunk.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/robofriends/static/media/reportWebVitals.d41d8cd9.bin"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "81ca8744f9db4b9719854e07f6ecc2ae",
+    "url": "/robofriends/index.html"
+  }
+];
